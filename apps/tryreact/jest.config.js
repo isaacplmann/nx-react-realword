b2b12cf6158c28d2cd9cbd9dd8627dd48a5933f4
Isaac Mann <isaacplmann+git@gmail.com>
2019-03-04 09:28:32 -0500

    initial commit

---
+++ apps/tryreact/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: "tryreact",
+  preset: "../../jest.config.js",
+  transform: {
+    "^.+\\.[tj]sx?$": "ts-jest"
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "html"],
+  coverageDirectory: "../../coverage/apps/tryreact/",
+  snapshotSerializers: [
+    "jest-preset-angular/AngularSnapshotSerializer.js",
+    "jest-preset-angular/HTMLCommentSerializer.js"
+  ]
+};
